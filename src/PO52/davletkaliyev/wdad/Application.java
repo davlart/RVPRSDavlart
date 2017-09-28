637249918cc1ae9cdb7ecce52a76e7e9bc12adf2
ArthurArt <davlartprivate@mail.ru>
2017-09-28 11:43:34 +0300

    Added entry point

---
+++ src/PO52/davletkaliyev/wdad/Application.java
@@ -0,0 +1,10 @@
+package PO52.davletkaliyev.wdad;
+
+/**
+ * Created by ArthurArt on 28.09.2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("I'm Arthur Davletkaliyev, and I'm not a monkey");
+    }
+}
