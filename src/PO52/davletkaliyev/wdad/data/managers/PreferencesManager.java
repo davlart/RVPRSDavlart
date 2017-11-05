7106b2e995d12488767f430d6a32799ec6c4e7db
ArthurArt <davlartprivate@mail.ru>
2017-11-06 01:42:56 +0300

    Обновляем удаленный репозиторий (push) и создаем пулл-реквест.

--- src/PO52/davletkaliyev/wdad/data/managers/PreferencesManager.java
+++ src/PO52/davletkaliyev/wdad/data/managers/PreferencesManager.java
@@ -3,6 +3,7 @@ package PO52.davletkaliyev.wdad.data.managers;
 /**
  * Created by ArthurArt on 29.10.2017.
  */
+import PO52.davletkaliyev.wdad.utils.PreferencesConstantManager;
  import org.w3c.dom.Document;
  import org.w3c.dom.Element;
  import org.w3c.dom.Node;
@@ -16,11 +17,13 @@ package PO52.davletkaliyev.wdad.data.managers;
  import javax.xml.transform.dom.DOMSource;
  import javax.xml.transform.stream.StreamResult;
  import java.io.File;
+ import java.util.Enumeration;
+ import java.util.Properties;
 
 public class PreferencesManager {
      private static volatile PreferencesManager instance;
      private Document doc;
-     private static final String FILE_PATH="src\\PO52\\Pecherin\\wdad\\resources\\configuration\\appconfig.xml";
+     private static final String FILE_PATH="src\\PO52\\davletkaliyev\\wdad\\resources\\configuration\\appconfig.xml";
 
     private PreferencesManager() throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -39,67 +42,135 @@ public class PreferencesManager {
         return instance;
     }
 
+    public void setProperty(String key, String value) {
+        getElement(key).setTextContent(value);
+    }
+
+    public String getProperty(String key) {
+        return getElement(key).getTextContent();
+    }
+
+    public void setProperties(Properties prop) {
+        Enumeration elements = prop.elements();
+        Enumeration keys = prop.keys();
+        while (elements.hasMoreElements()) {
+            getElement((String) elements.nextElement()).setTextContent((String) keys.nextElement());
+        }
+    }
+
+    public Properties getProperties() {
+        Properties prop = new Properties();
+        String[] keys = new String[]{
+                PreferencesConstantManager.CREATEREGISTRY,
+                PreferencesConstantManager.REGISTRYADDRESS,
+                PreferencesConstantManager.REGISTRYPORT,
+                PreferencesConstantManager.POLICYPATH,
+                PreferencesConstantManager.USECODEBASEONLY,
+                PreferencesConstantManager.CLASSPROVIDER
+        };
+
+        for (String key : keys) {
+            setProperty(key, getElement(key).getTextContent());
+        }
+
+        return prop;
+    }
+
+    public void addBindedObject(String name, String className) {
+        Element element=(Element) doc.createElement("bindedobject");
+        element.setAttribute("class",className);
+        element.setAttribute("name",name);
+        getElement("rmi").appendChild(element);
+    }
+
+    public void removeBindedObject(String name) {
+                NodeList nodeList = doc.getElementsByTagName("bindedobject");
+                Element element;
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                       element = (Element) nodeList.item(i);
+                       if (element.getAttribute("name").equals(name))
+                            {
+                                        getElement("rmi").removeChild(element);
+                       }
+                   }
+           }
+
+
     private Element getElement(String nameField) {
         NodeList nodeList = doc.getElementsByTagName(nameField);
+        String[] field = nameField.split("\\.");
+        nodeList = doc.getElementsByTagName(field[field.length - 1]);
         Node node = nodeList.item(0);
         return (Element) node;
     }
 
     //region Createregistry
+    @Deprecated
     public String getCreateregistry() {
         return getElement("createregistry").getTextContent();
     }
 
+    @Deprecated
     public void setCreateregistry(String value) {
         getElement("createregistry").setTextContent(value);
     }
      //endregion
 
     //region Registryaddress
+    @Deprecated
     public String getRegistryaddress() {
         return getElement("registryaddress").getTextContent();
     }
 
+    @Deprecated
     public void setRegistryaddress(String value) {
         getElement("registryaddress").setTextContent(value);
     }
      //endregion
 
     //region Registryport
+    @Deprecated
     public int getRegistryport() {
         return Integer.parseInt(getElement("registryport").getTextContent());
     }
 
+    @Deprecated
     public void setRegistryport(int value) {
         getElement("registryport").setTextContent(String.valueOf(value));
     }
     //endregion
 
     //region Policypath
+    @Deprecated
     public String getPolicypath() {
         return getElement("policypath").getTextContent();
     }
 
+    @Deprecated
     public void setPolicypath(String value) {
         getElement("policypath").setTextContent(value);
     }
      //endregion
 
     //region Usecodebaseonly
+    @Deprecated
     public String getUsecodebaseonly() {
         return getElement("usecodebaseonly").getTextContent();
     }
 
+    @Deprecated
     public void setUsecodebaseonly(String value) {
         getElement("usecodebaseonly").setTextContent(value);
     }
      //endregion
 
     //region Classprovider
+    @Deprecated
     public String getClassprovider() {
         return getElement("classprovider").getTextContent();
     }
 
+    @Deprecated
     public void setClassprovider(String value) {
         getElement("classprovider").setTextContent(value);
     }
